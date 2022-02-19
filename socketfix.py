6e5f107002e9b6333f41da0f69e6c740ee51d86b
derankfatti1108 <61102998+derankfatti1108@users.noreply.github.com>
2022-02-19 10:25:13 -0500

    Delete socketfix.py

--- socketfix.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-#this was simple to patch fucking socket methods have fun
-os.system("netstat -n -p | grep SYN_REC | awk '{print $5}' | awk -F: '{print $1}' | sort | uniq >> black.txt")
-ips = open("black.txt", "r").readlines()
-for ip in ips:
-	os.system(f"iptables -I INPUT -s {ip} -j DROP")
